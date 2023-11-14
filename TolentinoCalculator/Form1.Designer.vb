1b61966ba09521cddaaacfe4e2280f56f22804f9
Tolentino-JP <johnpaultolentino5@gmail.com>
2023-11-14 23:31:49 +0800

    Add project files.

---
+++ TolentinoCalculator/Form1.Designer.vb
@@ -0,0 +1,368 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnNegative = New Button()
+        btn0 = New Button()
+        btnDecimal = New Button()
+        btnEqual = New Button()
+        btn1 = New Button()
+        btn2 = New Button()
+        btn3 = New Button()
+        btnAdd = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn7 = New Button()
+        btn6 = New Button()
+        btn8 = New Button()
+        btnSubtract = New Button()
+        btn9 = New Button()
+        btnMultiply = New Button()
+        btnAC = New Button()
+        btnC = New Button()
+        Button19 = New Button()
+        btnBack = New Button()
+        input = New Label()
+        total = New Label()
+        SuspendLayout()
+        ' 
+        ' btnNegative
+        ' 
+        btnNegative.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnNegative.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnNegative.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnNegative.Location = New Point(4, 400)
+        btnNegative.Name = "btnNegative"
+        btnNegative.Size = New Size(84, 61)
+        btnNegative.TabIndex = 0
+        btnNegative.Text = "+/-"
+        btnNegative.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn0.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn0.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn0.Location = New Point(88, 400)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(84, 61)
+        btn0.TabIndex = 1
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btnDecimal
+        ' 
+        btnDecimal.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnDecimal.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnDecimal.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnDecimal.Location = New Point(172, 400)
+        btnDecimal.Name = "btnDecimal"
+        btnDecimal.Size = New Size(84, 61)
+        btnDecimal.TabIndex = 1
+        btnDecimal.Text = "."
+        btnDecimal.UseVisualStyleBackColor = True
+        ' 
+        ' btnEqual
+        ' 
+        btnEqual.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnEqual.BackColor = SystemColors.Highlight
+        btnEqual.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnEqual.FlatStyle = FlatStyle.Flat
+        btnEqual.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnEqual.Location = New Point(256, 400)
+        btnEqual.Name = "btnEqual"
+        btnEqual.Size = New Size(84, 61)
+        btnEqual.TabIndex = 1
+        btnEqual.Text = "="
+        btnEqual.UseVisualStyleBackColor = False
+        ' 
+        ' btn1
+        ' 
+        btn1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn1.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn1.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn1.Location = New Point(4, 339)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(84, 61)
+        btn1.TabIndex = 0
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btn2
+        ' 
+        btn2.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn2.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn2.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn2.Location = New Point(88, 339)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(84, 61)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn3.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn3.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn3.Location = New Point(172, 339)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(84, 61)
+        btn3.TabIndex = 1
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnAdd.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnAdd.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnAdd.Location = New Point(256, 339)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(84, 61)
+        btnAdd.TabIndex = 1
+        btnAdd.Text = "+"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn4.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn4.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn4.Location = New Point(4, 278)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(84, 61)
+        btn4.TabIndex = 0
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn5.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn5.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn5.Location = New Point(88, 278)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(84, 61)
+        btn5.TabIndex = 1
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn7.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn7.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn7.Location = New Point(4, 217)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(84, 61)
+        btn7.TabIndex = 0
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn6.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn6.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn6.Location = New Point(172, 278)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(84, 61)
+        btn6.TabIndex = 1
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn8.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn8.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn8.Location = New Point(88, 217)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(84, 61)
+        btn8.TabIndex = 1
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btnSubtract
+        ' 
+        btnSubtract.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnSubtract.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnSubtract.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnSubtract.Location = New Point(256, 278)
+        btnSubtract.Name = "btnSubtract"
+        btnSubtract.Size = New Size(84, 61)
+        btnSubtract.TabIndex = 1
+        btnSubtract.Text = "-"
+        btnSubtract.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btn9.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btn9.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btn9.Location = New Point(172, 217)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(84, 61)
+        btn9.TabIndex = 1
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnMultiply.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnMultiply.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnMultiply.Location = New Point(256, 217)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(84, 61)
+        btnMultiply.TabIndex = 1
+        btnMultiply.Text = "x"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnAC
+        ' 
+        btnAC.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnAC.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnAC.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnAC.Location = New Point(4, 156)
+        btnAC.Name = "btnAC"
+        btnAC.Size = New Size(84, 61)
+        btnAC.TabIndex = 0
+        btnAC.Text = "AC"
+        btnAC.UseVisualStyleBackColor = True
+        ' 
+        ' btnC
+        ' 
+        btnC.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnC.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnC.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnC.Location = New Point(88, 156)
+        btnC.Name = "btnC"
+        btnC.Size = New Size(84, 61)
+        btnC.TabIndex = 1
+        btnC.Text = "C"
+        btnC.UseVisualStyleBackColor = True
+        ' 
+        ' Button19
+        ' 
+        Button19.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Button19.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        Button19.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        Button19.Location = New Point(172, 156)
+        Button19.Name = "Button19"
+        Button19.Size = New Size(84, 61)
+        Button19.TabIndex = 1
+        Button19.Text = "÷"
+        Button19.UseVisualStyleBackColor = True
+        ' 
+        ' btnBack
+        ' 
+        btnBack.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        btnBack.FlatAppearance.BorderColor = SystemColors.ActiveBorder
+        btnBack.Font = New Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point)
+        btnBack.Location = New Point(256, 156)
+        btnBack.Name = "btnBack"
+        btnBack.Size = New Size(84, 61)
+        btnBack.TabIndex = 1
+        btnBack.Text = "⌫"
+        btnBack.UseVisualStyleBackColor = True
+        ' 
+        ' input
+        ' 
+        input.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        input.Font = New Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        input.Location = New Point(4, 84)
+        input.Name = "input"
+        input.Size = New Size(336, 68)
+        input.TabIndex = 2
+        input.TextAlign = ContentAlignment.MiddleRight
+        ' 
+        ' total
+        ' 
+        total.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        total.Font = New Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point)
+        total.Location = New Point(106, 58)
+        total.Name = "total"
+        total.Size = New Size(234, 26)
+        total.TabIndex = 3
+        total.TextAlign = ContentAlignment.MiddleRight
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveBorder
+        ClientSize = New Size(343, 463)
+        Controls.Add(total)
+        Controls.Add(input)
+        Controls.Add(btnBack)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnAdd)
+        Controls.Add(Button19)
+        Controls.Add(btn9)
+        Controls.Add(btn3)
+        Controls.Add(btnC)
+        Controls.Add(btnSubtract)
+        Controls.Add(btn8)
+        Controls.Add(btnEqual)
+        Controls.Add(btn6)
+        Controls.Add(btnAC)
+        Controls.Add(btn2)
+        Controls.Add(btn7)
+        Controls.Add(btnDecimal)
+        Controls.Add(btn5)
+        Controls.Add(btn1)
+        Controls.Add(btn4)
+        Controls.Add(btn0)
+        Controls.Add(btnNegative)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "Tolentino Calculator"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnNegative As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnDecimal As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnAC As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents input As Label
+    Friend WithEvents total As Label
+End Class
---
+++ TolentinoCalculator/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        input.Text = input.Text + "1"
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        input.Text = input.Text + "2"
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        input.Text = input.Text + "3"
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        input.Text = input.Text + "4"
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        input.Text = input.Text + "5"
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        input.Text = input.Text + "6"
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        input.Text = input.Text + "7"
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        input.Text = input.Text + "8"
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        input.Text = input.Text + "9"
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+
+        If input.Text = "" Then
+            input.Text = ""
+        Else
+            input.Text = input.Text + "0"
+        End If
+
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+
+        Dim _input As String = input.Text
+        If _input = "" Then
+
+        Else
+            Dim _output As String = _input.Remove(_input.Length - 1, 1)
+            input.Text = _output
+        End If
+
+
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        input.Text = ""
+        total.Text = ""
+    End Sub
+
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        input.Text = ""
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        total.Text = total.Text + input.Text + " + "
+        input.Text = ""
+    End Sub
+
+    Private Sub btnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        total.Text = total.Text + input.Text + " - "
+        input.Text = ""
+    End Sub
+End Class
